d3716f5ce0a1125f868dd78dc5ab53c4184a12f5
fergomap <gomezapariciofernando@gmail.com>
2020-01-31 13:01:25 +0100

    Chat example client.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { FunctionComponent, useEffect, useState, FormEvent } from 'react';
+import './App.scss';
+import axios from 'axios';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App: FunctionComponent = () => {
+    const [messages, setMessages] = useState<string[]>([]);
+    const [newMessage, setNewMessage] = useState<string>('');
+
+    useEffect(() => {
+        const chatListener = new EventSource('http://localhost:8080/messages');
+
+        chatListener.onmessage = (event) => {
+            setMessages(JSON.parse(event.data));
+        };
+    }, []);
+
+    const sendMessage = (e: FormEvent<HTMLFormElement>): void => {
+        e.preventDefault();
+
+        if (newMessage.length) {
+            axios.post('http://localhost:8080/send-message', {message: newMessage});
+            setNewMessage('');
+        }
+    };
+
+    return <div className="app">
+        <div className="header">
+            <h1>Chat app</h1>
+        </div>
+        <div className="messages">
+            {messages.map((message: string, index: number) => <p key={index}>{message}</p>)}
+        </div>
+        <form noValidate={true} onSubmit={sendMessage}>
+            <input value={newMessage} onChange={e => setNewMessage(e.target.value)} placeholder="Type a message" />
+            <button type="submit">SEND</button>
+        </form>
+    </div>;
 }
 
 export default App;
